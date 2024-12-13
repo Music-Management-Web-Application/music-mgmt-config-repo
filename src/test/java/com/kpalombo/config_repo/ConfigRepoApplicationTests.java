a03069cf5180c92beda3bc12a14b7c6c35007ce1
kmpalombo91 <kmpalombo91@gmail.com>
2024-12-13 14:27:07 -0600

    adding files for config-repo

---
+++ src/test/java/com/kpalombo/config_repo/ConfigRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpalombo.config_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
